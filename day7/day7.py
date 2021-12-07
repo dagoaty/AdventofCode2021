1f0a0e239602575332e87edbd862b79182dc85a9
Iain J. Watson <iain@ij-watson.co.uk>
2021-12-07 12:58:54 +0000

    Day 7 solution

---
+++ day7/day7.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import sys
+from typing import List, Dict
+
+def get_inputs(file: str) -> List[int]:
+    inputs: List[int] = []
+    f = open(file, "r")
+    for line in f:
+        inputs = ([int(n) for n in line.rstrip().split(',')])
+    return inputs
+
+
+def fuelCalc(curPos: int, destPos: int) -> int:
+    return abs(curPos - destPos)
+
+
+def thirstyFuelCalc(curPos: int, destPos: int) -> int:
+    return sum(range(1, abs(curPos - destPos)+1))
+
+
+def getLimit(inputs: List[int]) -> int:
+    return sorted(inputs)[-1]
+
+
+def part1(inputs: List[int]) -> None:
+    limit: int = getLimit(inputs)
+    fuelNeeded: Dict[int, int] = {}
+    for destPos in range(limit):
+        fuelNeeded[destPos] = sum([fuelCalc(n, destPos) for n in inputs])
+    leastFuelDest: int = min(fuelNeeded, key=lambda k: fuelNeeded.get(k, 0))
+    print("Part 1: %s requires %s" % (leastFuelDest, fuelNeeded[leastFuelDest]))
+
+
+def part2(inputs: List[int]) -> None:
+    limit: int = getLimit(inputs)
+    fuelNeeded: Dict[int, int] = {}
+    for destPos in range(limit):
+        fuelNeeded[destPos] = sum([thirstyFuelCalc(n, destPos) for n in inputs])
+    leastFuelDest: int = min(fuelNeeded, key=lambda k: fuelNeeded.get(k, 0))
+    print("Part 2: %s requires %s" % (leastFuelDest, fuelNeeded[leastFuelDest]))
+
+
+def main() -> None:
+    filename = sys.argv[1]
+    inputs: List[int] = get_inputs(filename)
+    part1(inputs)
+    part2(inputs)
+
+
+if __name__ == "__main__":
+    main()
